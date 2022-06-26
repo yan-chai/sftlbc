82e95f38646ce197e6b6d7cf311a4a88b87b8660
yan-chai <51705852+yan-chai@users.noreply.github.com>
2022-06-25 22:36:10 -0700

    empty spring project

---
+++ src/main/java/com/example/sftlbc/SftlbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.sftlbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftlbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SftlbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sftlbc/SftlbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sftlbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftlbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
