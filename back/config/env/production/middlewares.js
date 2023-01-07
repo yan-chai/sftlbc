2acd67c60d5a0a387ddb736ac38df7b7da097009
yan-chai <haoyuyan742@gmail.com>
2023-01-06 18:56:41 -0800

    Update middlewares.js

--- back/config/env/production/middlewares.js
+++ back/config/env/production/middlewares.js
@@ -1,47 +1,46 @@
 module.exports = [
-    'strapi::errors',
-    /* Replace 'strapi::security', with this snippet */
-    /* Beginning of snippet */
-    {
-      name: 'strapi::security',
-      config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-          directives: {
-            'connect-src': ["'self'", 'https:'],
-            'img-src': [
-              "'self'",
-              'data:',
-              'blob:',
-              'dl.airtable.com',
-              'us-west-1.us-west-1.amazonaws.com',
-            ],
-            'media-src': [
-              "'self'",
-              'data:',
-              'blob:',
-              'dl.airtable.com',
-              'us-west-1.s3.us-west-1.amazonaws.com',
-            ],
-            upgradeInsecureRequests: null,
-          },
+  'strapi::errors',
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'sftlbc.s3.us-west-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'sftlbc.s3.us-west-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
         },
       },
     },
-    {
-      name: 'strapi::cors',
-      config: {
-        enabled: true,
-        headers: '*',
-        origin: ['https://sftlbc-3nphj.ondigitalocean.app']
-      }
-    },
-    'strapi::poweredBy',
-    'strapi::logger',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
-  ];
-  
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['https://sftlbc-3nphj.ondigitalocean.app', "http://localhost:1337"]
+    }
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
